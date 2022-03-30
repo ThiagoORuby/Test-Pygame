c6afa160a95de0b466c2a52764cf3bb7f126d4fa
ThiagoORuby <trs3@aluno.ifal.edu.br>
2022-03-30 12:35:37 -0300

    hello

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hello, world" << endl;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << "\nWrite somethinggggg: " << endl;
+		int a;
+		cin >> a;
+		cout << a << endl; 
+	}
+	return 0;
+}
+
---
+++ helloworld.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+
+void main() {
+	print("Hello, world");
+
+	int id = 10;
+
+	for (var i = 0; i < id; i++) {
+		print("hi $i");
+	}
+}
+
+
+
+
+
+
+
+
